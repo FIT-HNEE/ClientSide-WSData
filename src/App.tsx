fbf42811e7ece2592973d1ca53b77f0b6c6ab21c
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-28 07:06:04 +0200

    Routes for sign in sign up and user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter, Switch, Link } from 'react-router-dom';
 import './App.css';
+import SignIn from './pages/User/SignIn';
+import SignUp from './pages/User/SignUp';
+import User from './pages/User/User';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>SignIn</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              {/* <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>SignIn</Link>
+              </li> */}
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                </li>
+                <li className="nav-item">
+                <Link className="nav-link" to={"/me"}>User</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+
+        <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={SignIn} />
+            <Route path="/sign-in" component={SignIn} />
+              <Route path="/sign-up" component={SignUp} />
+              <Route path="/me" component={User} />
+          </Switch>
+        </div>
+        </div>
+        
+      </div>
+   </BrowserRouter>
   );
 }
 
