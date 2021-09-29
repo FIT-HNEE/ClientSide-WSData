cd8091ffe350586873a4c600cedcb709686fab6c
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-09-29 02:15:35 +0200

    Fetching all the users route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter, Switch, Link } from 'react-router-dom';
 import './App.css';
+import AllUsers from './pages/User/AllUsers';
 import SignIn from './pages/User/SignIn';
 import SignUp from './pages/User/SignUp';
 import User from './pages/User/User';
@@ -13,10 +14,7 @@ function App() {
           <div className="container">
           <Link className="navbar-brand" to={"/sign-in"}>SignIn</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              {/* <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>SignIn</Link>
-              </li> */}
+            <ul className="navbar-nav ml-auto">              
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
                 </li>
@@ -35,6 +33,7 @@ function App() {
             <Route path="/sign-in" component={SignIn} />
               <Route path="/sign-up" component={SignUp} />
               <Route path="/me" component={User} />
+               <Route path="/allUsers" component={AllUsers} />
           </Switch>
         </div>
         </div>
