c5891da56e6b29abcbe2f4451b7980b430570f67
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-06 19:51:50 +0200

    Search bar using filter option and mui

--- src/pages/User/AllUsers.tsx
+++ src/pages/User/AllUsers.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import TableContainer from '@mui/material/TableContainer';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 /* interface IUSERS {
     data?: Array<any>
@@ -8,7 +15,9 @@ import { Button } from 'react-bootstrap';
 
 export default class AllUsers extends React.Component {
     state = {
-        data: []
+        data: [],
+        filteredRows: [],
+        searched: ''
     }
 
     componentDidMount = async () => {
@@ -16,22 +25,87 @@ export default class AllUsers extends React.Component {
         await axios.get('http://localhost:4000/api/users',
             {headers: { Authorization: `JWT ${accessToken}` }})
             .then(response => {
-                console.log('getData', response)               
+                console.log('AllUsersData', response)               
                 this.setState({                         
-                   data: response.data
+                    data: response.data,
+                    filteredRows: response.data
                 })
                 
                
             })
     }
+
+    requestSearch = (event: any) => {
+
+        let value = event.target.value.toLowerCase();
+        
+        const filteredRows = this.state.data && this.state.data.filter((row: any) => {
+        
+            return row.firstName.toLowerCase().includes(value) || row.lastName.toLowerCase().includes(value) || row.email.toLowerCase().includes(value)
+            
+        });
+        
+        console.log('Result', filteredRows);
+        
+        this.setState({
+
+            filteredRows
+
+        })
+
+  };
+
+    /* cancelSearch = () => {
+        this.setState({
+           searched: ""
+       })   
+    this.requestSearch(this.state.searched);
+  }; */
+
     render() {
+        
         return (
             <div>
-                {this.state.data.map((dt: any) => {
-                    return(<h1> {dt.email} </h1>)
+                <Paper>
+                    <div className="App">
+                        <div style={{ margin: '0 auto', marginTop: '10%' }}>
+                        <label>Search:</label>
+                        <input type="text" onChange={this.requestSearch} />
+                        </div>
+                    </div>
+                    <TableContainer>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>firstName</TableCell>
+                                <TableCell align="right">Last Name</TableCell>
+                                <TableCell align="right">email</TableCell>
+                                
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {this.state.filteredRows &&this.state.filteredRows.map((row: any) => {
+                    return (
+                        <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.firstName}
+                  </TableCell>
+                  <TableCell align="right">{row.lastName}</TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  {/* <TableCell align="right">{row.isAdmin}</TableCell>
+                  <TableCell align="right">{row.confirmation}</TableCell> */}
+                </TableRow>)
                     
                 })}
-               <Button onClick = {()=>(localStorage.clear())} > LOGOUT </Button>
+                            </TableBody>
+                        </Table>
+                        </TableContainer>
+                </Paper> 
+                <Button
+                    variant='contained'
+                    color='primary'                    
+                    onClick={() => (localStorage.clear())} > LOGOUT </Button>
+              
             
             </div>
             
