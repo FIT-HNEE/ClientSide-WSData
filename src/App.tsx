0136f5ae8f24cdbd47f6b379f3aacc569ee79852
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-07 12:07:39 +0200

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import NavBar from "./components/NavBar";
-
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <NavBar />      
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+            
    </>
   );
 }
