3ab51582a0c52fe5402d11a21ca4a204be88e8d1
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-14 16:07:44 +0200

    Using date picker from material ui for date inputs

--- src/pages/WeatherData/WeatherDataFetching.tsx
+++ src/pages/WeatherData/WeatherDataFetching.tsx
@@ -8,107 +8,119 @@ import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
 import FormGroup from '@mui/material/FormGroup';
 import Select from '@mui/material/Select';
-//import { History } from 'history';
-/* interface ChildComponentProps {
-  history : History
-  
-} */
-/* interface HomeProps {
-  history: RouteComponentProps["history"];
-  location: RouteComponentProps['location'];
-  match: RouteComponentProps['match'];
-} */
-
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Stack from '@mui/material/Stack';
+import moment from 'moment'
+import { parseISO } from 'date-fns/esm';
 
 interface WeatherDataCredentials {
     StationName?: string,
-    StartDay?: string,
-    EndDay?: string
+    StartDay?: any,
+    EndDay?: any
 }
  
-
-
 class WeatherDataFetching extends React.Component<RouteComponentProps, WeatherDataCredentials> {
-    constructor(props: RouteComponentProps) {
-        super(props)
-    
-        this.state = {
-        StationName: '',
-        StartDay: '',
-        EndDay: ''
-    }    
-        
-    }   
-    
 
-  onButtonClick = async (event: React.FormEvent) => {
-  event.preventDefault();
-  const response = await axios.post("http://localhost:4000/api/weatherData", {
-    StationName: this.state.StationName,
-    StartDay: this.state.StartDay,
-    EndDay: this.state.EndDay,
-  });
-    const data = await response.data
-  this.props.history.push({
-    pathname: "/data",
-    state: {
-      WeatherData: data,
-    },
-  });
-};
-  render() {
+  constructor(props: RouteComponentProps) {
       
+    super(props)
+    
+    
+    this.state = {
+          
+      StationName: '',
+      
+      StartDay: '',
+        
+      EndDay: ''
+        
+    }  
+        
+  }
+  
+    
+  onButtonClick = async (event: React.FormEvent) => {
+    event.preventDefault();    
+    const response = await axios.post("http://localhost:4000/api/weatherData", {
+    
+      StationName: this.state.StationName,      
+      StartDay: this.state.StartDay,    
+      EndDay: this.state.EndDay,
+    
+    });
+    
+    const data = await response.data
+
+    this.props.history.push({    
+      pathname: "/WeatherData",      
+      state: {      
+        WeatherData: data,        
+      },      
+    });
+    
+  };
+
+  render() {    
     return (
-      <Box
+      <Box        
         component="form"
         sx={{
           '& .MuiTextField-root': { m: 1, width: '25ch' },
         }}
         noValidate
         autoComplete="off"
-      >
+      >        
         <div>
           <h3>Weather Data Search</h3>
-                <FormGroup >
-                    <InputLabel id="demo-simple-select-label">Station Name</InputLabel>
-  <Select    
-    value={this.state.StationName}
-    label="Station Name"
-    onChange={(event) => this.setState({              
+          
+          <FormGroup >            
+            <InputLabel id="demo-simple-select-label">Station Name</InputLabel>
+            <Select              
+              value={this.state.StationName}              
+              label="Station Name"              
+              onChange={(event) => this.setState({      
                 StationName: event.target.value                
               })}
-  >
-    <MenuItem value="Buche">Buche</MenuItem>
-    {/* <MenuItem value={20}>Twenty</MenuItem>
-    <MenuItem value={30}>Thirty</MenuItem> */}
-  </Select>
-            {/* <TextField        
-              required
-              label="StationName"
-              type="text"                
-              value={this.state.StationName}              
-              onChange={(event) => this.setState({              
-                StationName: event.target.value                
-              })}              
-            />     */}        
-            <TextField              
-              required              
-              label="StartDay"              
-              type="text"          
-              value={this.state.StartDay}              
-              onChange={(event) => this.setState({              
-                StartDay: event.target.value                
-              })}              
-                    />
-                    <TextField              
-              required              
-              label="StartDay"              
-              type="text"          
-              value={this.state.EndDay}              
-              onChange={(event) => this.setState({              
-                EndDay: event.target.value                
-              })}              
-            />
+            >              
+              <MenuItem value="Buche">Buche</MenuItem>    
+            </Select>
+
+            <LocalizationProvider dateAdapter={DateAdapter}>
+              <Stack spacing={3}>
+                <DesktopDatePicker                  
+                  //openTo="year"
+                  //views={["year", "month", "day"]}
+                  label="Start Date"
+                  //inputFormat="dd/MM/yyyy"
+                  value={this.state.StartDay}
+                  disableFuture={true}
+                  onChange={(date: any) => {
+                  this.setState({
+                    StartDay: moment(date).format('YYYY-MM-DD')   
+                  });  
+                  }}                  
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <DesktopDatePicker                  
+                  //openTo="year"
+                  //views={["year", "month", "day"]}
+                  label="End Date"                  
+                  //inputFormat="MM/dd/yyyy"                  
+                  disableFuture={true}
+                  value={this.state.EndDay}
+                  onChange= {(date: any) => {
+                  this.setState({                   
+                    EndDay: moment(date).format('YYYY-MM-DD')
+                    
+                  });                    
+                  }}
+                  minDate= {parseISO(this.state.StartDay)}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+               </Stack>
+            </LocalizationProvider>   
             <Button onClick={this.onButtonClick} variant='contained' color='primary'>Search Data</Button>
           </FormGroup>
         </div>        
