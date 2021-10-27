bc9fbf554e83a0798e786211c63b568adbda26b2
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-27 21:28:46 +0200

    Simple card with grid (responsive layout) using material ui

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default class HomePage extends Component {
+
+    
+    render() {
+        return (
+            <Container maxWidth="xl">
+                <Grid container rowSpacing={1} >
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                BBBBBB
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                            </Card>
+                    </Grid>
+
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                BBBBBB
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                            </Card>
+                    </Grid>
+                </Grid>
+            </Container>            
+        )
+    }
+}
