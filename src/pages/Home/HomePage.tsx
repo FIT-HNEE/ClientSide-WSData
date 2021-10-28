820e0ae262f0cf088f009ef407b46cd2e002e4fc
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-28 22:18:23 +0200

    Grid with having first rows with cards having spacing 5 and small and extra small screen with one column and on other screens with two columns
    For the second row there should be only one column throughout the screens.

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -13,8 +13,57 @@ export default class HomePage extends Component {
     render() {
         return (
             <Container maxWidth="xl">
-                <Grid container rowSpacing={1} >
+                <Grid container spacing={5} >
+
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                BBBBBB
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>                        
+                    </Grid>
+
+                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                Word of the Day
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                BBBBBB
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                            </Card>
+                    </Grid>
+
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         <Card sx={{ minWidth: 275 }}>
                             <CardContent>
                                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -61,7 +110,9 @@ export default class HomePage extends Component {
                             </CardActions>
                             </Card>
                     </Grid>
+
                 </Grid>
+                
             </Container>            
         )
     }
