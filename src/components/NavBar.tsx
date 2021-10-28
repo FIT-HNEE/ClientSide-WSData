aadd114cdd5905d8952546d6ea5d726ec56bc6ae
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-10-29 00:09:42 +0200

    Navbar burger menu icon on down sizing from md

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, RouteComponentProps, withRouter  } from 'react-router-dom';
 import AllUsers from '../pages/User/AllUsers';
 import SignIn from '../pages/User/SignIn';
 import SignUp from '../pages/User/SignUp';
 import User from '../pages/User/User';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  Avatar,
+  useMediaQuery,
+  useTheme,  
+  IconButton,
+   Drawer
+} from '@mui/material'
+import { makeStyles } from "@mui/styles";
+import MenuIcon from '@mui/icons-material/Menu';
 import image from '../images/FBG.jpg'
-import Button from '@mui/material/Button';
 import WeatherDataFetching from "../pages/WeatherData/WeatherDataFetching";
 import WeatherData from "../pages/WeatherData/WeatherData";
 import Figures from "./dataVisualization/Figures";
-import Avatar from '@mui/material/Avatar';
 import HomePage from "../pages/Home/HomePage";
+const drawerWidth = 240;
+const useStyles = makeStyles(() => ({
+  drawerContainer: {},
+  
 
-const NavBar: React.FC<RouteComponentProps> = (props) => {    
+drawer: {
+  width: drawerWidth,
+  flexShrink: 0,
+  maxHeight: '20px'
+  //backgroundColor: "rgba(0,0,0,0.6)" Don't target here
+},
+drawerPaper: {
+  width: drawerWidth,
+  backgroundColor: "rgba(120,120,120,0.2)", //target here
+  maxHeight: '20px'
+},
+    iconButtonContainer: {
+      marginLeft: 'auto',
+      color: 'white',
+    },
+
+    menuIconToggle: {
+      fontSize: '2rem',
+    },
+  }));
+
+const NavBar: React.FC<RouteComponentProps> = (props) => {
+  
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const theme = useTheme(); 
+  const isMatch = useMediaQuery(theme.breakpoints.down('md'));
+
+  //Css
+  const classes = useStyles();
+  
 
     return (
         <>
@@ -26,10 +67,60 @@ const NavBar: React.FC<RouteComponentProps> = (props) => {
           
           <Avatar alt="Remy Sharp" src={image} />
       
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Weather Data APP
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Button color="inherit" href="/" >Weather Data APP</Button>            
               </Typography>
-              {(() => {
+              {isMatch ? (
+                <>
+                  <Drawer
+                    anchor='right'
+                    className={classes.drawer}
+                    variant="persistent"
+                    classes={{ paper: classes.drawerContainer }}
+                    onClose={() => setOpenDrawer(false)}
+                    open={openDrawer}
+                    onClick={() => setOpenDrawer(true)}>
+                  {(() => {
+                const accessToken = localStorage.getItem('accessToken');
+                console.log('Access Token here', accessToken)
+
+                if (accessToken && accessToken !== null) {
+                  return (
+                    <> 
+                      <Button color="inherit" href="/me" onClick={() => setOpenDrawer(false)}>User</Button>                      
+                      <Button color="inherit" href="/AllUsers" onClick={() => setOpenDrawer(false)}>AllUsers</Button>
+                      <Button
+                    variant='contained'
+                    color='primary'                    
+                        onClick={async() => {
+                          await localStorage.clear()
+                          await props.history.push("/sign-in")
+                          await setOpenDrawer(false);                          
+                        }}> LOGOUT </Button>
+                    </>                    
+                  )
+                } else {
+                  return (
+                    <> 
+                      <Button color="inherit" href="/" onClick={() => setOpenDrawer(false)} >Home</Button>
+                      <Button color="inherit" href="/sign-in" onClick={() => setOpenDrawer(false)} >Login</Button>
+                      <Button color="inherit" href="/WeatherDataFetching" onClick={() => setOpenDrawer(false)} >SearchWeatherData</Button>                      
+                      <Button color="inherit" href="/sign-up" onClick={() => setOpenDrawer(false)} >Sign up</Button>
+                  </> 
+                  )
+                                   
+                }
+              })
+              ()}
+                  </Drawer>
+                  <IconButton
+                    className={classes.iconButtonContainer}
+                    onClick={() => setOpenDrawer(!openDrawer)}
+                    disableRipple>
+                    <MenuIcon className={classes.menuIconToggle} />
+                  </IconButton>
+                </>) : (
+                  (() => {
                 const accessToken = localStorage.getItem('accessToken');
                 console.log('Access Token here', accessToken)
 
@@ -49,17 +140,19 @@ const NavBar: React.FC<RouteComponentProps> = (props) => {
                   )
                 } else {
                   return (
-                    <> 
+                    <>
+                      <Button color="inherit" href="/">Home</Button>
                       <Button color="inherit" href="/sign-in" >Login</Button>
-                      <Button color="inherit" href="/WeatherDataFetching" >SearchWeatherData</Button>
-                      <Button color="inherit" href="/WeatherData" >WeatherData</Button>
+                      <Button color="inherit" href="/WeatherDataFetching" >SearchWeatherData</Button>                      
                       <Button color="inherit" href="/sign-up" >Sign up</Button>
                   </> 
                   )
                                    
                 }
               })
-              ()}
+              ()
+              )}
+              
                        
                         
         </Toolbar>
