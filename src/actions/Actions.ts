79d30ba3fd3b4fc4cc072168a63d6407812432a8
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-12 18:01:39 +0100

    Actions for getting past seven days data and Querying weather data

---
+++ src/actions/Actions.ts
@@ -0,0 +1,65 @@
+import {Dispatch} from "redux";
+import { PAST_WEATHER_DATA_FAIL, PAST_WEATHER_DATA_LOADING, PAST_WEATHER_DATA_SUCCESS, PWDDispatchTypes } from "./PastWeatherActionTypes";
+import {Query_WEATHER_DATA_LOADING, Query_WEATHER_DATA_SUCCESS, QWDDispatchTypes, Query_WEATHER_DATA_FAIL} from './QueryWeatherDataTypes'
+import axios from "axios";
+
+// interface WeatherDataCredentials {
+//     StationName?: string,
+//     StartDay?: any,
+//     EndDay?: any
+// }
+
+export const GetPWData = () => async (dispatch: Dispatch<PWDDispatchTypes>) => {
+    try {
+
+        dispatch({
+            type: PAST_WEATHER_DATA_LOADING,            
+        });        
+
+        const resp = await axios.get("http://localhost:4000/api/weatherData/lastSevenDays");        
+        const data = await resp.data
+        
+        dispatch({                
+            type: PAST_WEATHER_DATA_SUCCESS,            
+            payload: data                
+        });
+        
+    } catch (error) {
+
+        console.log('ERROR', error)
+        dispatch({
+            type: PAST_WEATHER_DATA_FAIL            
+        })        
+    }    
+}
+
+export const GetQWData = (StationName: any, StartDay: any, EndDay: any) => async ( dispatch:Dispatch<QWDDispatchTypes>) => {
+    
+    try {                
+
+        dispatch({
+            type: Query_WEATHER_DATA_LOADING,            
+        });
+        const response = await axios.post("http://localhost:4000/api/weatherData", {
+    
+      StationName: StationName,      
+      StartDay: StartDay,    
+      EndDay: EndDay,
+    
+    });
+    
+    const data = await response.data
+        
+        dispatch({                
+            type: Query_WEATHER_DATA_SUCCESS,            
+            payload: data                
+        });
+        
+    } catch (error) {
+
+        console.log('ERROR', error)
+        dispatch({
+            type: Query_WEATHER_DATA_FAIL            
+        })        
+    }    
+}
