bca8da5c2834362f2aea26c5ccb57be2ca29d904
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-21 16:01:06 +0100

    Update for Log In data

--- src/reducers/RootReducer.ts
+++ src/reducers/RootReducer.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import PastWeatherDataReducer from "./PastWeatherDataReducer";
 import QueryWDReducer from "./QueryWDReducer";
+import LogInDataReducer from "./LogInDataReducer";
 
 const RootReducer = combineReducers({
   dataPWD: PastWeatherDataReducer,
-  dataQWD: QueryWDReducer
+  dataQWD: QueryWDReducer,
+  dataLogIn: LogInDataReducer
 });
 
 export default RootReducer;
