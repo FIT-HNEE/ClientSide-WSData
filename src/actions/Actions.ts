12868e422fbb22d82dc72131acea452038c0dc44
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-21 15:58:27 +0100

    Action for Log In of the User

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -1,6 +1,7 @@
 import {Dispatch} from "redux";
 import { PAST_WEATHER_DATA_FAIL, PAST_WEATHER_DATA_LOADING, PAST_WEATHER_DATA_SUCCESS, PWDDispatchTypes } from "./PastWeatherActionTypes";
-import {Query_WEATHER_DATA_LOADING, Query_WEATHER_DATA_SUCCESS, QWDDispatchTypes, Query_WEATHER_DATA_FAIL} from './QueryWeatherDataTypes'
+import { Query_WEATHER_DATA_LOADING, Query_WEATHER_DATA_SUCCESS, QWDDispatchTypes, Query_WEATHER_DATA_FAIL } from './QueryWeatherDataTypes';
+import {LOGIN_DATA_LOADING, LOGIN_DATA_SUCCESS, LOGIN_DATA_FAIL, LogInDispatchTypes} from './LoginActionTypes'
 import axios from "axios";
 
 // interface WeatherDataCredentials {
@@ -60,6 +61,38 @@ export const GetQWData = (StationName: any, StartDay: any, EndDay: any) => async
         console.log('ERROR', error)
         dispatch({
             type: Query_WEATHER_DATA_FAIL            
+        })        
+    }    
+}
+
+export const LogInData = (email: any, password: any) => async (dispatch: Dispatch<LogInDispatchTypes>) => {
+    try {
+
+        dispatch({
+            type: LOGIN_DATA_LOADING,            
+        });
+        
+        const response = await axios.post('http://localhost:4000/api/users/login',
+        {        
+          email: email,   
+          password: password
+            }, { withCredentials: true })
+        
+        await localStorage.setItem('accessToken', response.data.tokens.accessToken)
+        await localStorage.setItem('refreshToken', response.data.tokens.refreshToken)        
+            
+        const data = response.data     
+        
+        dispatch({                
+            type: LOGIN_DATA_SUCCESS,            
+            payload: data                
+        });
+        
+    } catch (error) {
+
+        console.log('ERROR', error)
+        dispatch({
+            type: LOGIN_DATA_FAIL            
         })        
     }    
 }
