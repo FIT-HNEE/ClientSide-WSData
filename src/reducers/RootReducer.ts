32f88c724006c230e0b8730031f880c6cb5ce75f
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-22 14:37:48 +0100

    Update for User data

--- src/reducers/RootReducer.ts
+++ src/reducers/RootReducer.ts
@@ -3,12 +3,14 @@ import PastWeatherDataReducer from "./dataReducers/PastWeatherDataReducer";
 import QueryWDReducer from "./dataReducers/QueryWDReducer";
 import LogInDataReducer from "./dataReducers/LogInDataReducer";
 import SignUpDataReducer from './dataReducers/SignUpDataReducer';
+import UserDataReducer from "./dataReducers/UserDataReducer";
 
 const RootReducer = combineReducers({
   PWdata: PastWeatherDataReducer,
   QWdata: QueryWDReducer,
   LogIndata: LogInDataReducer,
-  SignUpdata: SignUpDataReducer
+  SignUpdata: SignUpDataReducer,
+  UserData: UserDataReducer
 });
 
 export default RootReducer;
