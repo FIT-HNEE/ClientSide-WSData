9f021b1bb3b9efd8036329fe5f9525921232ad3d
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-24 06:51:21 +0100

    Using function for search bar in Actions

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -4,13 +4,9 @@ import { Query_WEATHER_DATA_LOADING, Query_WEATHER_DATA_SUCCESS, QWDDispatchType
 import { LOGIN_DATA_LOADING, LOGIN_DATA_SUCCESS, LOGIN_DATA_FAIL, LogInDispatchTypes } from './types/LoginActionTypes';
 import { SIGNUP_DATA_LOADING, SIGNUP_DATA_FAIL, SIGNUP_DATA_SUCCESS, SignUpDispatchTypes } from './types/SignUpActionTypes';
 import { USER_DATA_LOADING, USER_DATA_SUCCESS, USER_DATA_FAIL, UserDataDispatchTypes} from './types/UserDataActionTypes'
+import { USERS_DATA_LOADING, USERS_DATA_SUCCESS, USERS_DATA_FAIL, UsersDataDispatchTypes, FILTER_BY_VALUE,SEARCH, UsersDataType} from './types/UsersDataActionTypes'
 import axios from "axios";
 
-// interface WeatherDataCredentials {
-//     StationName?: string,
-//     StartDay?: any,
-//     EndDay?: any
-// }
 
 export const GetPWData = () => async (dispatch: Dispatch<PWDDispatchTypes>) => {
     try {
@@ -157,3 +153,59 @@ export const GetUserData = () => async (dispatch: Dispatch<UserDataDispatchTypes
         })        
     }    
 }
+
+export const GetUsersData = (value:any) => async (dispatch: Dispatch<UsersDataDispatchTypes>) => {
+    try {
+
+        dispatch({
+            type: USERS_DATA_LOADING,            
+        });
+        
+        const accessToken = sessionStorage.getItem('accessToken');
+
+        const resp = await axios.get('http://localhost:4000/api/users',
+            {headers: { Authorization: `JWT ${accessToken}` }});        
+        const data = await resp.data
+        
+        dispatch({                
+            type: USERS_DATA_SUCCESS,            
+            payload: data                
+        });
+        /* dispatch({
+    type: FILTER_BY_VALUE,
+    payload: {
+      filteredData: data.filter((row: any) => {        
+            return row.firstName.toLowerCase().includes(value) || row.lastName.toLowerCase().includes(value) || row.email.toLowerCase().includes(value)
+            
+      })
+    },
+  }); */
+        
+    } catch (error) {
+
+        console.log('ERROR-ACTION', error)
+        dispatch({
+            type: USERS_DATA_FAIL            
+        })        
+    }    
+}
+
+export const search = (value: any) => {
+  return {type: SEARCH, value};
+}
+export const filterByValue = (data:any, value:any) => (dispatch: Dispatch<UsersDataDispatchTypes>) => {
+  dispatch({
+    type: FILTER_BY_VALUE,
+    payload: {
+      filteredData: data.filter((row: any) => {        
+            return row.firstName.toLowerCase().includes(value) || row.lastName.toLowerCase().includes(value) || row.email.toLowerCase().includes(value)
+            
+      })
+    },
+  });
+};
+
+
+
+
+
