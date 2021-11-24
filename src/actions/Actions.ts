3ee0fb66a26a754d3b35ee12567e50c9ad912575
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-24 16:30:57 +0100

    Users Data fetching function

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -4,7 +4,7 @@ import { Query_WEATHER_DATA_LOADING, Query_WEATHER_DATA_SUCCESS, QWDDispatchType
 import { LOGIN_DATA_LOADING, LOGIN_DATA_SUCCESS, LOGIN_DATA_FAIL, LogInDispatchTypes } from './types/LoginActionTypes';
 import { SIGNUP_DATA_LOADING, SIGNUP_DATA_FAIL, SIGNUP_DATA_SUCCESS, SignUpDispatchTypes } from './types/SignUpActionTypes';
 import { USER_DATA_LOADING, USER_DATA_SUCCESS, USER_DATA_FAIL, UserDataDispatchTypes} from './types/UserDataActionTypes'
-import { USERS_DATA_LOADING, USERS_DATA_SUCCESS, USERS_DATA_FAIL, UsersDataDispatchTypes, FILTER_BY_VALUE,SEARCH, UsersDataType} from './types/UsersDataActionTypes'
+import { USERS_DATA_LOADING, USERS_DATA_SUCCESS, USERS_DATA_FAIL, UsersDataDispatchTypes} from './types/UsersDataActionTypes'
 import axios from "axios";
 
 
@@ -154,7 +154,7 @@ export const GetUserData = () => async (dispatch: Dispatch<UserDataDispatchTypes
     }    
 }
 
-export const GetUsersData = (value:any) => async (dispatch: Dispatch<UsersDataDispatchTypes>) => {
+export const GetUsersData = () => async (dispatch: Dispatch<UsersDataDispatchTypes>) => {
     try {
 
         dispatch({
@@ -171,15 +171,7 @@ export const GetUsersData = (value:any) => async (dispatch: Dispatch<UsersDataDi
             type: USERS_DATA_SUCCESS,            
             payload: data                
         });
-        /* dispatch({
-    type: FILTER_BY_VALUE,
-    payload: {
-      filteredData: data.filter((row: any) => {        
-            return row.firstName.toLowerCase().includes(value) || row.lastName.toLowerCase().includes(value) || row.email.toLowerCase().includes(value)
-            
-      })
-    },
-  }); */
+        
         
     } catch (error) {
 
@@ -189,21 +181,6 @@ export const GetUsersData = (value:any) => async (dispatch: Dispatch<UsersDataDi
         })        
     }    
 }
-
-export const search = (value: any) => {
-  return {type: SEARCH, value};
-}
-export const filterByValue = (data:any, value:any) => (dispatch: Dispatch<UsersDataDispatchTypes>) => {
-  dispatch({
-    type: FILTER_BY_VALUE,
-    payload: {
-      filteredData: data.filter((row: any) => {        
-            return row.firstName.toLowerCase().includes(value) || row.lastName.toLowerCase().includes(value) || row.email.toLowerCase().includes(value)
-            
-      })
-    },
-  });
-};
 
 
 
