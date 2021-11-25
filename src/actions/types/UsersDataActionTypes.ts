82149da9ee6c2d9f86b7ebbb520184f17eac62b0
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-11-25 20:19:18 +0100

    Update UsersDataActionTypes.ts

--- src/actions/types/UsersDataActionTypes.ts
+++ src/actions/types/UsersDataActionTypes.ts
@@ -1,12 +1,9 @@
 export const USERS_DATA_LOADING = "USERS_DATA_LOADING";
 export const USERS_DATA_FAIL = "USERS_DATA_FAIL";
 export const USERS_DATA_SUCCESS = "USERS_DATA_SUCCESS";
-export const FILTER_BY_VALUE = "FILTER_BY_VALUE";
-export const SEARCH = 'SEARCH';
 
 export type UsersDataType = {
-    data: []
-    
+    data: []    
 }
 export interface UsersDataLoading {
 
