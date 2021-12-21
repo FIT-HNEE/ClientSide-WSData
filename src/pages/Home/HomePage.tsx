024157fcb82fc124de374dee90c8772498cc1d56
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-12-21 01:19:25 +0100

    weather forecast component update

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -9,6 +9,7 @@ import Chart from 'chart.js/auto'
 import moment from 'moment'
 import {connect} from "react-redux";
 import {GetPWData} from '../../actions/Actions'
+import WeatherAPI from '../WeatherForecast/WeatherAPI';
 
 interface LastSevenDaysWeatherProps {
     GetPWData: Function
@@ -183,20 +184,7 @@ class HomePage extends React.Component<LastSevenDaysWeatherProps> {
                     <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
                         <Card sx={{ minWidth: 275 }}>
                             <CardContent>
-                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                Word of the Day
-                                </Typography>
-                                <Typography variant="h5" component="div">
-                                BBBBBB
-                                </Typography>
-                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                adjective
-                                </Typography>
-                                <Typography variant="body2">
-                                well meaning and kindly.
-                                <br />
-                                {'"a benevolent smile"'}
-                                </Typography>
+                               <WeatherAPI />
                             </CardContent>
                             <CardActions>
                                 <Button size="small">Learn More</Button>
