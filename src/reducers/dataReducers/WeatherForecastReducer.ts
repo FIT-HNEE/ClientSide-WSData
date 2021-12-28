d0476505914e27fbbbc6c3e2019537ffb0b2297c
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-12-28 01:33:06 +0100

    Create WeatherForecastReducer.ts

---
+++ src/reducers/dataReducers/WeatherForecastReducer.ts
@@ -0,0 +1,39 @@
+import { Weather_Forecast_FAIL, Weather_Forecast_LOADING, Weather_Forecast_SUCCESS, FWDDispatchTypes, FWDType } from "../../actions/types/WeatherForecastActionTypes"
+
+interface  DefaultStateI {
+    loading: boolean,
+    FWDType?: FWDType,
+    error: boolean
+};
+
+const defaultState: DefaultStateI = {
+    loading: false,
+    error: false
+};
+
+const PastWeatherDataReducer = (state: DefaultStateI = defaultState, action: FWDDispatchTypes) : DefaultStateI   => {
+    switch (action.type) {
+        case Weather_Forecast_FAIL:
+            return {
+                loading: false,
+                error: true
+            }
+        case Weather_Forecast_LOADING:            
+            return {
+                loading: true,
+                error: false
+            }
+        case Weather_Forecast_SUCCESS:
+            console.log(action.payload)
+            return {
+                loading: false,
+                error: false,
+                FWDType: action.payload
+            }
+
+        default:
+            return state
+    }
+}
+
+export default PastWeatherDataReducer;
