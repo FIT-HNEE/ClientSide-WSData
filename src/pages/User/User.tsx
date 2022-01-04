4fcc9d8ab42ecdf053bc1248e1692818feb47202
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2022-01-04 05:23:18 +0100

    Update User.tsx

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -3,7 +3,6 @@ import { RouteComponentProps, withRouter  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { GetUserData } from '../../actions/Actions'
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
